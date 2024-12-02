7078efe22efcb9f8a446c3606c5ed8ca79804dbf
Slowerdive <balashik.ac@gmail.com>
2024-12-02 18:24:40 +0300

    Update self_chain.py

--- 5_iterators/self_chain/self_chain.py
+++ 5_iterators/self_chain/self_chain.py
@@ -1,12 +1,33 @@
-from typing import Generator, Iterable, TypeVar
+from typing import Generator, Iterable, TypeVar, Iterator
 
 T = TypeVar("T")
 
-
 def chain(*iterables: Iterable[T]) -> Generator[T, None, None]:
-    """Пишите ваш код здесь"""
-
+    for iterable in iterables:
+        for element in iterable:
+            yield element
 
 class Chain:
     def __init__(self, *iterables: Iterable[T]):
-        """Реализуйте класс ниже"""
+        self.iterables = iterables
+        self.current_iter = iter(self.iterables)
+        self.current = None
+        self._advance_iterable()
+
+    def _advance_iterable(self):
+        try:
+            self.current = iter(next(self.current_iter))
+        except StopIteration:
+            self.current = None
+
+    def __iter__(self) -> Iterator[T]:
+        return self
+
+    def __next__(self) -> T:
+        if self.current is None:
+            raise StopIteration
+        try:
+            return next(self.current)
+        except StopIteration:
+            self._advance_iterable()
+            return self.__next__()
