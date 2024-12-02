6bf323e2b0ef4a4fabf1ff22b4bb6149bcfb7faa
Slowerdive <balashik.ac@gmail.com>
2024-12-02 18:14:22 +0300

    Update main.py

--- 2_dicts/3_task/main.py
+++ 2_dicts/3_task/main.py
@@ -1,12 +1,16 @@
 def format_phone(phone_number: str) -> str:
-    """Функция возвращает отформатированный телефон.
 
-    Args:
-        phone_number: исходный телефон
+    digits = ''.join(filter(str.isdigit, phone_number))
 
-    Returns:
-        отформатированный телефон
-    """
-    formatted_phone_number = ""
+    if len(digits) == 11 and digits.startswith('89'):
+        formatted_phone_number = f"8 ({digits[1:4]}) {digits[4:7]}-{digits[7:9]}-{digits[9:11]}"
+    elif len(digits) == 10 and digits.startswith('9'):
+        formatted_phone_number = f"8 ({digits[:3]}) {digits[3:6]}-{digits[6:8]}-{digits[8:10]}"
+    elif len(digits) == 11 and digits.startswith('79'):
+        formatted_phone_number = f"8 ({digits[1:4]}) {digits[4:7]}-{digits[7:9]}-{digits[9:11]}"
+    elif len(digits) == 12 and digits.startswith('79'):
+        formatted_phone_number = f"8 ({digits[2:5]}) {digits[5:8]}-{digits[8:10]}-{digits[10:12]}"
+    else:
+        formatted_phone_number = digits
 
     return formatted_phone_number
