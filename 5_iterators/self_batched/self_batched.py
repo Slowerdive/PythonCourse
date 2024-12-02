983de220c0d4e854300c81ebd9383042e7696093
Slowerdive <balashik.ac@gmail.com>
2024-12-02 18:24:17 +0300

    Update self_batched.py

--- 5_iterators/self_batched/self_batched.py
+++ 5_iterators/self_batched/self_batched.py
@@ -1,12 +1,25 @@
-from typing import Generator, Iterable, TypeVar
+from typing import Generator, Iterable, TypeVar, Iterator, Tuple
 
 T = TypeVar("T")
 
-
-def batched(obj: Iterable[T], n: int) -> Generator[tuple[T], None, None]:
-    """Пиши свой код здесь."""
-
+def batched(obj: Iterable[T], n: int) -> Generator[Tuple[T, ...], None, None]:
+    it = iter(obj)
+    while True:
+        batch = tuple(next(it, None) for _ in range(n))
+        if not batch or batch == (None,) * n:
+            break
+        yield batch[:len(batch) - batch.count(None)]
 
 class Batched:
     def __init__(self, obj: Iterable[T], n: int):
-        """Реализуй этот класс."""
+        self.obj = iter(obj)
+        self.n = n
+
+    def __iter__(self) -> Iterator[Tuple[T, ...]]:
+        return self
+
+    def __next__(self) -> Tuple[T, ...]:
+        batch = tuple(next(self.obj, None) for _ in range(self.n))
+        if not batch or batch == (None,) * self.n:
+            raise StopIteration
+        return batch[:len(batch) - batch.count(None)]
