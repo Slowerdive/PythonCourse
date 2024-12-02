d86e176448228702672c3623b86b3564ac38a0fa
Slowerdive <balashik.ac@gmail.com>
2024-12-02 18:25:03 +0300

    Update self_cycle.py

--- 5_iterators/self_cycle/self_cycle.py
+++ 5_iterators/self_cycle/self_cycle.py
@@ -1,12 +1,24 @@
-from typing import Generator, Iterable, TypeVar
+from typing import Generator, Iterable, TypeVar, Iterator
 
 T = TypeVar("T")
 
-
 def cycle(obj: Iterable[T]) -> Generator[T, None, None]:
-    """Пишите ваш код здесь."""
-
+    saved = list(obj)
+    while saved:
+        for element in saved:
+            yield element
 
 class Cycle:
     def __init__(self, obj: Iterable[T]):
-        """Реализуйте класс"""
+        self.saved = list(obj)
+        self.index = 0
+
+    def __iter__(self) -> Iterator[T]:
+        return self
+
+    def __next__(self) -> T:
+        if not self.saved:
+            raise StopIteration
+        result = self.saved[self.index]
+        self.index = (self.index + 1) % len(self.saved)
+        return result
