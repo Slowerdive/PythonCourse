9abe02dce9fa24bafaffdd2e144c4ce5eb9c9997
Slowerdive <balashik.ac@gmail.com>
2025-01-02 17:48:50 +0300

    Update main.py

--- 7_final_task/main.py
+++ 7_final_task/main.py
@@ -3,6 +3,7 @@ from history import save_history, print_history
 from location import get_location
 from datetime import datetime
 
+
 def handle_choice(choice):
     if choice == '1':
         city = input("Введите название города: ")
@@ -33,6 +34,7 @@ def handle_choice(choice):
         return False
     return True
 
+
 def main():
     while True:
         print("\nМеню:")
@@ -40,9 +42,15 @@ def main():
         print("2. Показать историю запросов")
         print("3. Получить погоду по текущему местоположению")
         print("4. Выход")
-        choice = input("Выберите действие: ")
-        if not handle_choice(choice):
-            break
+
+        try:
+            choice = input("Выберите действие: ")
+            if not handle_choice(choice):
+                break
+        except Exception as e:
+            print(f"Произошла ошибка: {e}")
+            print("Попробуйте снова или выберите другой пункт меню.")
+
 
 if __name__ == '__main__':
     main()
